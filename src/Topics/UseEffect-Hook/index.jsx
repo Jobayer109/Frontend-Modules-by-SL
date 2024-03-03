fefe78728a0b7f9670cc9f465a8bef0d78ff6dbd
user.email <jobayer.ahmed109@gmail.com>
2024-03-03 20:19:28 +0600

    simply explored the useEffect Hook

--- src/Topics/UseEffect-Hook/index.jsx
+++ src/Topics/UseEffect-Hook/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UseEffectHook = () => {
-  return <div>UseEffectHook</div>;
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+
+  useEffect(() => {
+    if (count === 5) {
+      setLock(true);
+    }
+  }, [count]);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Count: {count}</h1>
+      <button disabled={lock} onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
 };
 
 export default UseEffectHook;
