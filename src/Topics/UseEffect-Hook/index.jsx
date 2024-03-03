5df5587d96d766c8c370757bb23b2057f28449bb
user.email <jobayer.ahmed109@gmail.com>
2024-03-03 21:11:01 +0600

    Button disabled count time down

--- src/Topics/UseEffect-Hook/index.jsx
+++ src/Topics/UseEffect-Hook/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 const UseEffectHook = () => {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
+  const [timeCount, setTimeCount] = useState(5);
 
   useEffect(() => {
     if (count === 5) {
@@ -10,11 +11,28 @@ const UseEffectHook = () => {
     }
   }, [count]);
 
+  useEffect(() => {
+    let time = null;
+
+    if (lock) {
+      time = setInterval(() => {
+        if (timeCount !== 0) {
+          setTimeCount(timeCount - 1);
+        } else {
+          setCount(0);
+          setLock(false);
+          clearTimeout(time);
+          return;
+        }
+      }, 1000);
+    }
+  }, [lock, timeCount]);
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Count: {count}</h1>
       <button disabled={lock} onClick={() => setCount(count + 1)}>
-        Increment
+        Increment {`(${timeCount})`}
       </button>
     </div>
   );
